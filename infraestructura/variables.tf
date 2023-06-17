41d7af746b9030499a103f68cd2b4d3ced1d5a29
nikodyulger <nikolasvdulgerov@gmail.com>
2023-06-17 19:17:14 +0200

    Variables para las regiones

--- infraestructura/variables.tf
+++ infraestructura/variables.tf
@@ -1,3 +1,11 @@
 variable "blog_domain_name" {
   default = "recetas-para-sobrevivir.com"
 }
+
+variable "spain_region" {
+  default = "eu-south-2"
+}
+
+variable "north_virginia_region" {
+  default = "us-east-1"
+}
