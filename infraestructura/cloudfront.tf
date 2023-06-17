aabc2a0b53828067473c37ed9910acc2c1a99abf
nikodyulger <nikolasvdulgerov@gmail.com>
2023-06-17 19:18:33 +0200

    Migrado de OAI a OAC Cloudfront, eliminado root_object

--- infraestructura/cloudfront.tf
+++ infraestructura/cloudfront.tf
@@ -2,29 +2,29 @@ locals {
   s3_origin_id = "blog_bucket_origin_${aws_s3_bucket.blog_bucket.id}"
 }
 
-resource "aws_cloudfront_origin_access_identity" "blog_oai_s3_cdn" {
-  comment = "OAI Cloudfront para acceder al blog_bucket"
+resource "aws_cloudfront_origin_access_control" "cloudfront_s3_oac" {
+  name                              = "CloudFront S3 OAC"
+  description                       = "CloudFront S3 OAC"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
 resource "aws_cloudfront_distribution" "blog_distribution" {
 
   origin {
-    domain_name = aws_s3_bucket.blog_bucket.bucket_regional_domain_name
-    origin_id   = local.s3_origin_id
-
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.blog_oai_s3_cdn.cloudfront_access_identity_path
-    }
+    domain_name              = aws_s3_bucket.blog_bucket.bucket_regional_domain_name
+    origin_id                = local.s3_origin_id
+    origin_access_control_id = aws_cloudfront_origin_access_control.cloudfront_s3_oac.id
   }
 
   aliases = [
     var.blog_domain_name
   ]
-  enabled             = true
-  is_ipv6_enabled     = true
-  price_class         = "PriceClass_100"
-  comment             = "CDN para el blog_bucket"
-  default_root_object = "index.html"
+  enabled         = true
+  is_ipv6_enabled = true
+  price_class     = "PriceClass_100"
+  comment         = "CDN para el blog_bucket"
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
@@ -54,8 +54,8 @@ resource "aws_cloudfront_distribution" "blog_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.blog_ssl_certificate.arn
-    ssl_support_method  = "sni-only"
+    acm_certificate_arn      = aws_acm_certificate.blog_ssl_certificate.arn
+    ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
 
