72f0d70c1cf5f4a95032bd2e195048139c84a4cf
nikodyulger <nikolasvdulgerov@gmail.com>
2023-06-17 19:17:03 +0200

    Añadido backend para tfstate

--- infraestructura/main.tf
+++ infraestructura/main.tf
@@ -7,14 +7,21 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket                 = "backend-terraform-proyectos"
+    key                    = "blog-recetas/terraform.tfstate"
+    region                 = "eu-south-2"
+    skip_region_validation = true
+  }
 }
 
 provider "aws" {
-  shared_config_files      = ["/Users/nikodyulger/.aws/config"]
-  shared_credentials_files = ["/Users/nikodyulger/.aws/credentials"]
+  alias  = "spain_region"
+  region = var.spain_region
 }
 
 provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
+  alias  = "north_virginia_region"
+  region = var.north_virginia_region
 }
