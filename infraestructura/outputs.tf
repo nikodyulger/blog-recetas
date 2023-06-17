522daa6d18db8a6e004b0e0f804354df3d8d581f
nikodyulger <nikolasvdulgerov@gmail.com>
2023-06-17 19:18:47 +0200

    Formateo outputs

--- infraestructura/outputs.tf
+++ infraestructura/outputs.tf
@@ -1,7 +1,7 @@
 output "route53_domain" {
-  value = "${aws_route53_record.blog_distribution_domain.fqdn}"
+  value = aws_route53_record.blog_distribution_domain.fqdn
 }
 
 output "cdn_domain" {
-  value = "${aws_cloudfront_distribution.blog_distribution.domain_name}"
+  value = aws_cloudfront_distribution.blog_distribution.domain_name
 }
