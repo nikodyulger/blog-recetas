b6e91567e8e8cb7cb50ecbdd2611ee544673190a
nikodyulger <nikolasvdulgerov@gmail.com>
2023-06-17 19:17:40 +0200

    Nombre bucket, política y error_document

--- infraestructura/s3.tf
+++ infraestructura/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "blog_bucket" {
-  bucket = "blog-recetas-para-sobrevir"
+  bucket = "blog-recetas-para-sobrevivir"
 }
 
 resource "aws_s3_bucket_versioning" "blog_bucket_versioning" {
@@ -17,18 +17,29 @@ resource "aws_s3_bucket_website_configuration" "blog_website_config_bucket" {
   }
 
   error_document {
-    key = "error.html"
+    key = "404.html"
   }
 }
 
 data "aws_iam_policy_document" "blog_bucket_policy_data" {
   statement {
+    sid = "CloudfrontOAC"
+
     actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.blog_bucket.arn}/*"]
 
     principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.blog_oai_s3_cdn.iam_arn]
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+
+      values = [
+        aws_cloudfront_distribution.blog_distribution.arn
+      ]
     }
   }
 }
@@ -38,6 +49,6 @@ resource "aws_s3_bucket_policy" "blog_bucket_policy" {
   policy = data.aws_iam_policy_document.blog_bucket_policy_data.json
 
   depends_on = [
-    aws_cloudfront_origin_access_identity.blog_oai_s3_cdn
+    aws_cloudfront_distribution.blog_distribution
   ]
 }
